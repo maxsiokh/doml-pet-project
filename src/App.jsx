54b1359a86ecabcb1f1fd2f1f86a2bfd8a0cc068
Maxim <maxsiokh@gmail.com>
2025-01-06 19:02:33 +0200

    add styles for Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './Pages/Header/Header'
 
 function App() {
 	const [count, setCount] = useState(0)
 
 	return (
 		<>
+			<Header />
 			<h1 className='text-3xl font-bold underline text-yellow-800'>
 				Hello world!
 			</h1>
--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -1,5 +1,50 @@
 import React from 'react'
 
 export default function Header() {
-	return <div>Header</div>
+	return (
+		<div className='flex justify-between'>
+			<nav className='flex '>
+				<a href='/' className=' text-2xl flex items-center font-bold'>
+					<img src='logo.svg' className='w-6 pr-1' alt='' />
+					<span>DOML</span>
+				</a>
+
+				<div className='list-none flex items-center '>
+					<ul className='flex justify-between space-x-6 pl-3 text-base'>
+						<li>
+							<a className='' href=''>
+								Product
+							</a>
+						</li>
+						<li>
+							<a href=''>Team</a>
+						</li>
+						<li>
+							<a href=''>Enterprise</a>
+						</li>
+						<li>
+							<a href=''>Explore</a>
+						</li>
+						<li>
+							<a href=''>Marketplace</a>
+						</li>
+						<li>
+							<a href=''>Pricing</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+			<div className='flex justify-between'>
+				<input
+					className='bg-[#272F43] text-white placeholder-gray-400 rounded-md px-4 py-2 focus:outline-none'
+					type='text'
+					placeholder='Search'
+				/>
+				<div className='flex justify-between space-x-4 pl-4'>
+					<button>Sign In </button>
+					<button>Sign Up</button>
+				</div>
+			</div>
+		</div>
+	)
 }
