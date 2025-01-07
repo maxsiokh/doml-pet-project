10dc3d70be02958298e79d135a700400781da68f
Maxim <maxsiokh@gmail.com>
2025-01-07 14:14:08 +0200

    added line

---
+++ src/Components/Line/Line.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Line() {
+	return (
+		<div className=' h-px w-10/12 bg-gradient-to-r from-pink-600 via-blue-600 to-cyan-300'></div>
+	)
+}
--- src/Pages/Intro.jsx
+++ src/Pages/Intro.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
 import MainButton from '../Components/Button/MainButton'
+import Line from '../Components/Line/Line'
 
 export default function Intro() {
 	return (
 		<div className='flex items-center justify-between pt-10'>
 			<div
 				id='intro__text'
-				className='flex-col-reverse text-7xl leading-[1.3] text-left'
+				className='flex-col-reverse text-7xl leading-[1.3] text-left space-y-11'
 			>
-				<h1 className=' bg-gradient-to-r from-pink-600 via-[#5200FF] to-cyan-300 bg-clip-text text-transparent'>
-					AI Marketing.
-				</h1>
-				<h1 className=''>Optimized Reach.</h1>
+				<div>
+					<h1 className=' bg-gradient-to-r from-pink-600 via-[#5200FF] to-cyan-300 bg-clip-text text-transparent'>
+						AI Marketing.
+					</h1>
+					<h1 className=''>Optimized Reach.</h1>
+				</div>
+
 				<p className='text-2xl font-extralight'>
 					Our vision is to revolutionize the way brands and advertisers target,
 					reach
 				</p>
-				<MainButton  text='Get started' />
+				<Line />
+				<MainButton text='Get started' />
 			</div>
 
 			<img src='planeta.png' className='' alt='' />
